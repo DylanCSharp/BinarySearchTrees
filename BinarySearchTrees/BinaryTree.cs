a944389c210211e405234df26c2ea2718a0426a1
Dylan Cox <dylancox247@gmail.com>
2021-10-10 09:20:53 +0200

    tried the searching but not sure if it is correct

--- BinarySearchTrees/BinaryTree.cs
+++ BinarySearchTrees/BinaryTree.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace BinarySearchTrees
 {
@@ -8,7 +10,9 @@ namespace BinarySearchTrees
 
         public static BinaryTree binaryTree = new();
 
-        public static BinaryTree GetTrees()
+        public static Queue<Node> queue = new();
+
+        public static BinaryTree GetAllInTree()
         {
             binaryTree.Add(50);
             binaryTree.Add(30);
@@ -31,7 +35,7 @@ namespace BinarySearchTrees
 
         public bool Add(int value)
         {
-            Node before = null; 
+            Node before = null;
             Node after = Root;
             while (after != null)
             {
@@ -50,9 +54,9 @@ namespace BinarySearchTrees
                 }
             }
 
-            Node newNode = new() 
-            { 
-                Data = value 
+            Node newNode = new()
+            {
+                Data = value
             };
 
             if (Root == null)
@@ -71,6 +75,91 @@ namespace BinarySearchTrees
                 }
             }
             return true;
+        }
+
+        public Node Search(int value)
+        {
+            return Search(value, Root);
+        }
+
+        public Node Search(int value, Node parent)
+        {
+            if (parent != null)
+            {
+                if (value == parent.Data)
+                {
+                    return parent;
+                }
+                if (value < parent.Data)
+                {
+                    return Search(value, parent.LeftNode);
+                }
+                else
+                {
+                    return Search(value, parent.RightNode);
+                }
+            }
+            return null;
+        }
+
+        public void Remove(int value)
+        {
+            Root = Remove(Root, value);
+
+        }
+
+        public Node Remove(Node parent, int key)
+        {
+            if (parent == null)
+            {
+                return parent;
+            }
+            if (key < parent.Data)
+            {
+                parent.LeftNode = Remove(parent.LeftNode, key);
+            }
+            else if (key > parent.Data)
+            {
+                parent.RightNode = Remove(parent.RightNode, key);
+            }
+            else
+            {
+                if (parent.LeftNode == null)
+                {
+                    return parent.RightNode;
+                }
+                else if (parent.RightNode == null)
+                {
+                    return parent.LeftNode;
+                }
+                parent.Data = MinValue(parent.RightNode);
+                parent.RightNode = Remove(parent.RightNode, parent.Data);
+            }
+            return parent;
+        }
+
+        public static int MinValue(Node node)
+        {
+            int minv = node.Data;
+
+            while (node.LeftNode != null)
+            {
+                minv = node.LeftNode.Data;
+                node = node.LeftNode;
+            }
+
+            return minv;
+        }
+
+
+        public int GetTreeDepth()
+        {
+            return GetTreeDepth(Root);
+        }
+
+        public int GetTreeDepth(Node parent)
+        {
+            return parent == null ? 0 : Math.Max(GetTreeDepth(parent.LeftNode), GetTreeDepth(parent.RightNode)) + 1;
         }
 
         public void PreOrder(Node parent)
--- BinarySearchTrees/Program.cs
+++ BinarySearchTrees/Program.cs
@@ -6,18 +6,64 @@ namespace BinarySearchTrees
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("BST ICE ONE");
             Console.WriteLine("Pre-Order:");
-            BinaryTree.GetTrees().PreOrder(BinaryTree.GetTrees().Root);
+            BinaryTree.GetAllInTree().PreOrder(BinaryTree.GetAllInTree().Root);
             Console.WriteLine();
 
             Console.WriteLine("In Order:");
-            BinaryTree.GetTrees().InOrder(BinaryTree.GetTrees().Root);
+            BinaryTree.GetAllInTree().InOrder(BinaryTree.GetAllInTree().Root);
             Console.WriteLine();
 
             Console.WriteLine("Post-Order:");
-            BinaryTree.GetTrees().PostOrder(BinaryTree.GetTrees().Root);
-            Console.WriteLine();
+            BinaryTree.GetAllInTree().PostOrder(BinaryTree.GetAllInTree().Root);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("BST ICE TWO");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter an option: D for Delete , DS for Depth First Search, BS for Breadth-First Search");
+                Console.WriteLine("Full list will be re-added after a different option is selected");
+                string answer = Console.ReadLine();
+
+                if (answer is "D" or "d")
+                {
+                    Console.WriteLine("Enter the number of the node youd like to delete");
+                    BinaryTree.GetAllInTree().InOrder(BinaryTree.GetAllInTree().Root);
+                    Console.WriteLine();
+                    string numberToDelete = Console.ReadLine();
+                    BinaryTree.binaryTree.Remove(Convert.ToInt32(numberToDelete));
+                    Console.WriteLine($"{numberToDelete} deleted\nThe remaining numbers: ");
+                    BinaryTree.binaryTree.InOrder(BinaryTree.binaryTree.Root);
+                    Console.WriteLine();
+                }
+                else if (answer is "DS" or "ds" or "Ds" or "dS")
+                {
+                    Console.WriteLine("Enter a number you want to depth search");
+                    string numberToDepthSearch = Console.ReadLine();
+                    Console.Write("Pre-Order\t"); 
+                    BinaryTree.GetAllInTree().PreOrder(BinaryTree.GetAllInTree().Search(Convert.ToInt32(numberToDepthSearch)));
+                    Console.WriteLine();
+                    Console.Write("In Order \t"); 
+                    BinaryTree.GetAllInTree().InOrder(BinaryTree.GetAllInTree().Search(Convert.ToInt32(numberToDepthSearch)));
+                    Console.WriteLine();
+                    Console.Write("Post-Order \t");
+                    BinaryTree.GetAllInTree().PostOrder(BinaryTree.GetAllInTree().Search(Convert.ToInt32(numberToDepthSearch)));
+                    Console.WriteLine();
+
+                }
+                else if (answer is "BS" or "bs" or "Bs" or "bS")
+                {
+                    int numberToBreadthSearch = BinaryTree.GetAllInTree().GetTreeDepth();
+                 
+                    Console.WriteLine(numberToBreadthSearch);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input");
+                }
+            }
 
             Console.ReadLine();
         }
